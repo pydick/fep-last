4216edd0b26eb9a88dc355da4d692f582564d590
pydick <p1053192517@163.com>
2025-09-20 21:19:56 +0800

    fix init

--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,106 +1,118 @@
-<!-- 测试页面 -->
-<!-- 一个上传按钮 -->
+<script>
+import { reactive, ref } from 'vue'
+import protein3d_molstar from '@/components/molecule/protein3d_molstar.vue'
+import { data } from './data.js'
+
+export default {
+  name: 'ScreenDocking',
+  components: {
+    Protein3dMolstar: protein3d_molstar,
+  },
+  setup() {
+    const form = reactive({
+      is_isomer: true,
+      need_prot_process: true,
+      smiles_col: '',
+      input_tab: '数据库导入',
+      pdbid_url: '',
+      pdb_id: '',
+      pdb_dataset_id: '',
+      ligands_id: '',
+      pdbid_input: '',
+      pdbid_select: '',
+      protein_file: '',
+      pdb_name: '',
+      pdbfile_validate_status: '',
+      ligandfile_validate_status: '',
+      protein_chain: [],
+      het_group: [],
+      correct: true,
+      protonation: true,
+      charge: 'amber14/protein.ff14SB',
+      minimize_flag: 'atoms',
+      minimized: null,
+      missing_residue: [],
+      missing_residue_list: [],
+      pdb: 'pdb_file',
+      chain: [],
+      add_missing_residue: true,
+      addh: true,
+      modify_protonation: true,
+      ph: 7.4,
+      opt_hydrogen: true,
+      energy_min: true,
+      force_field: 'amber14/protein.ff14SB',
+
+      ligand_input_tab: '上传文件',
+      ligand_file: '',
+      ligand_smiles: '',
+      ligand_example_select: '',
+      Disconnect_group: true,
+      keep_large_fragment: true,
+      Ionization: 'set_pH',
+      Ionization_pH_main: 7.4,
+      Ionization_pH_sub: 0.2,
+      Tautomers: false,
+      Stereoisomers: 'retain',
+      isomer_limit: 5,
+      ligand_energy_minimize: 'MMFF94',
+
+      X_center: null,
+      Y_center: null,
+      Z_center: null,
+      X_dimension: null,
+      Y_dimension: null,
+      Z_dimension: null,
+      mode: 'semi',
+      distance: null,
+      scoring: 'carsidock',
+      rescoring: ['RTMS'],
+      num_pose: 1,
+      task_name: 'Docking Task',
+      // box_ligand: 'default',
+      box_ligand: 'A:KY9:1102',
+      box_changed_by_user: false,
+      irrelevant_waters: false,
+      het_group: [],
+      delete_water: [],
+    })
+    const theme = ref('light')
+    const if_show_box = ref(true)
+    const molstar3dRef = ref(null)
+    return { form, theme, if_show_box, molstar3dRef }
+  },
+  methods: {
+    show_protein() {
+      this.$refs.molstar3dRef.loadStructure(data, 'pdb')
+    },
+  },
+}
+</script>
+
 <template>
-  <div class="p-4">
-    <Button variant="default" class="flex items-center gap-2" @click="uploadFile">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" /><polyline points="17 8 12 3 7 8" /><line x1="12" y1="3" x2="12" y2="15" /></svg>
-      上传文件
-    </Button>
+  <div>
+    <div style="width: 800px; height: 800px">
+      <el-button @click="show_protein">
+        show_protein
+      </el-button>
+      <Protein3dMolstar
+        ref="molstar3dRef"
+        v-model:box_x="form.X_center"
+        v-model:box_y="form.Y_center"
+        v-model:box_z="form.Z_center"
+        v-model:l1="form.X_dimension"
+        v-model:l2="form.Y_dimension"
+        v-model:l3="form.Z_dimension"
+        v-model:ligand_select="form.box_ligand"
+        v-model:theme="theme"
+        v-model:if_changed_box="form.box_changed_by_user"
+        :if_show_box="if_show_box"
+      />
+    </div>
   </div>
 </template>
 
-<script setup>
-import { ref } from "vue";
-
-const encryptedData = ref('')
-const key = ref("XAEhg51aKKSMHgFUSQJr6hEkWv5gCYJ8j7kfQixCV8c=");
-
-function uploadFile() {
-  // 获取文件后进行解密
-  const file = document.createElement("input");
-  file.type = "file";
-  file.accept = ".pdf";
-  file.click();
-
-  file.onchange = async (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(file);
-    reader.onload = async () => {
-      try {
-        // 将 ArrayBuffer 转换为 Uint8Array
-        const arrayBuffer = reader.result;
-        const encryptedBytes = new Uint8Array(arrayBuffer);
-
-        // 从加密数据中提取IV(前16字节)和密文
-        const iv = encryptedBytes.slice(0, 16);
-        const ciphertext = encryptedBytes.slice(16);
-
-        // 将IV和密文转换为Base64格式的字符串
-        const ivBase64 = arrayBufferToBase64(iv.buffer);
-        const ciphertextBase64 = arrayBufferToBase64(ciphertext.buffer);
-
-        // 使用CryptoJS解密
-        const decrypted = await decryptFile(ciphertextBase64, key.value, ivBase64);
-
-        // 保存解密后的数据
-        const decryptedFile = new File([decrypted], "decrypted.pdf", { type: "application/pdf" });
-        const url = URL.createObjectURL(decryptedFile);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "decrypted.pdf";
-        a.click();
-      }
-      catch (error) {
-        console.error("解密失败:", error);
-        // alert('文件解密失败: ' + error.message)
-      }
-    };
-  };
-}
-
-// ArrayBuffer转Base64
-function arrayBufferToBase64(buffer) {
-  const binary = [];
-  const bytes = new Uint8Array(buffer);
-  for (let i = 0; i < bytes.byteLength; i++) {
-    binary.push(String.fromCharCode(bytes[i]));
-  }
-  return btoa(binary.join(""));
-}
-
-// Base64转ArrayBuffer
-function base64ToArrayBuffer(base64) {
-  const binaryString = atob(base64);
-  const bytes = new Uint8Array(binaryString.length);
-  for (let i = 0; i < binaryString.length; i++) {
-    bytes[i] = binaryString.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
-
-// 解密文件
-async function decryptFile(ciphertextBase64, keyBase64, ivBase64) {
-  // 将密钥和IV转换为WordArray格式
-  const keyWordArray = CryptoJS.enc.Base64.parse(keyBase64);
-  const ivWordArray = CryptoJS.enc.Base64.parse(ivBase64);
-
-  // 解密
-  const decrypted = CryptoJS.AES.decrypt(
-    ciphertextBase64,
-    keyWordArray,
-    {
-      iv: ivWordArray,
-      padding: CryptoJS.pad.Pkcs7,
-      mode: CryptoJS.mode.CBC,
-    },
-  );
-
-  // 转换为ArrayBuffer
-  const decryptedStr = decrypted.toString(CryptoJS.enc.Base64);
-  return new Uint8Array(base64ToArrayBuffer(decryptedStr));
-}
-</script>
+<style lang="scss" scoped></style>
 
 <route lang="yaml">
   meta:
